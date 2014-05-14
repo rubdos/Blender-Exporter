1fd283fca8f42a1e411aaf21ec5ee7e924625a5b
povmaniaco <p.alcaide@hotmail.com>
2014-05-14 23:58:29 +0200

    Create subclass for own material properties, need for add material nodes

--- ui/properties_yaf_material.py
+++ ui/properties_yaf_material.py
@@ -21,13 +21,12 @@
 import bpy
 from ..ui.ior_values import ior_list
 from bpy.types import Panel, Menu
-from bl_ui.properties_material import (MaterialButtonsPanel,
-                                       active_node_mat,
+from bl_ui.properties_material import (active_node_mat,
                                        check_material)
 
 #MaterialButtonsPanel.COMPAT_ENGINES = {'THEBOUNTY'}
 
-class THEBOUNTY_MaterialButtonsPanel():
+class TheBountyMaterialButtonsPanel():
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
     bl_context = "material"
@@ -38,24 +37,24 @@ class THEBOUNTY_MaterialButtonsPanel():
     def poll(cls, context):
         return context.material and (context.scene.render.engine in cls.COMPAT_ENGINES)
 
-class THEBOUNTY_MT_material_presets(Menu):
+class TheBountyMaterialPresets(Menu):
     bl_label = "Material Presets"
     preset_subdir = "thebounty/material"
     preset_operator = "script.execute_preset"
     COMPAT_ENGINES = {'THEBOUNTY'}
     draw = Menu.draw_preset
 
-class MaterialTypePanel(THEBOUNTY_MaterialButtonsPanel):
+class TheBountyMaterialTypePanel(TheBountyMaterialButtonsPanel):
     COMPAT_ENGINES = {'THEBOUNTY'}
 
     @classmethod
     def poll(cls, context):
         yaf_mat = context.material
         engine = context.scene.render.engine
-        return check_material(yaf_mat) and (yaf_mat.mat_type in cls.material_type) and (engine in cls.COMPAT_ENGINES)
+        return check_material(yaf_mat) and (yaf_mat.bounty.mat_type in cls.material_type) and (engine in cls.COMPAT_ENGINES)
 
 
-class MAT_PT_context_material(THEBOUNTY_MaterialButtonsPanel, Panel):
+class TheBountyContextMaterial(TheBountyMaterialButtonsPanel, Panel):
     bl_label = "Material"
     bl_options = {'HIDE_HEADER'}
     COMPAT_ENGINES = {'THEBOUNTY'}
@@ -70,17 +69,18 @@ class MAT_PT_context_material(THEBOUNTY_MaterialButtonsPanel, Panel):
     def draw(self, context):
         layout = self.layout
 
-        yaf_mat = context.material
+        mat = context.material # povman: no bounty here ??
+        #material = mat.bounty
         ob = context.object
         slot = context.material_slot
         space = context.space_data
 
         if ob:
             row = layout.row()
-            if bpy.app.version < (2, 65, 3 ):
-                row.template_list(ob, "material_slots", ob, "active_material_index", rows=2)
-            else:
-                row.template_list("MATERIAL_UL_matslots", "", ob, "material_slots", ob, "active_material_index", rows=2)
+            #if bpy.app.version < (2, 65, 3 ):
+            #    row.template_list(ob, "material_slots", ob, "active_material_index", rows=2)
+            #else:
+            row.template_list("MATERIAL_UL_matslots", "", ob, "material_slots", ob, "active_material_index", rows=2)
 
             col = row.column(align=True)
             col.operator("object.material_slot_add", icon='ZOOMIN', text="")
@@ -103,29 +103,33 @@ class MAT_PT_context_material(THEBOUNTY_MaterialButtonsPanel, Panel):
             #----------------
             # test for nodes
             #----------------
-            mat = context.material
-            if mat:
-                row.prop(mat, "use_nodes", icon='NODETREE', text="")
+            #ymat = context.material
+            #if ymat:
+            #   row.prop(ymat, "use_nodes", icon='NODETREE', text="")
             #----------------
             if slot:
                 row.prop(slot, "link", text="")
             else:
                 row.label()
 
-        elif yaf_mat:
+        elif mat:
             split.template_ID(space, "pin_id")
             split.separator()
 
-        if yaf_mat:
+        if mat:
+            row = layout.row()
+            row.operator("bounty.add_nodetree", icon='NODETREE')
             layout.separator()
-            layout.prop(yaf_mat, "mat_type") # expand true..
+            layout.prop(mat.bounty, "mat_type") # expand true..
+            
             row = layout.row(align=True)
-            row.menu("THEBOUNTY_MT_material_presets", text=bpy.types.THEBOUNTY_MT_material_presets.bl_label)
+            row.menu("TheBountyMaterialPresets", text=bpy.types.TheBountyMaterialPresets.bl_label)
             row.operator("bounty.material_preset_add", text="", icon='ZOOMIN')
             row.operator("bounty.material_preset_add", text="", icon='ZOOMOUT').remove_active = True
             #-------------------
             # test for nodes
             #-------------------
+            '''
             if mat.use_nodes:
                 row = layout.row()
                 row.label(text="", icon='NODETREE')
@@ -134,15 +138,15 @@ class MAT_PT_context_material(THEBOUNTY_MaterialButtonsPanel, Panel):
                 else:
                     row.label(text="No material node selected")
             #-------------------
-        
+            '''
 
-class YAF_MATERIAL_PT_preview(THEBOUNTY_MaterialButtonsPanel, Panel):
+class TheBountyMaterialPreview(TheBountyMaterialButtonsPanel, Panel):
     bl_label = "Preview" 
     bl_options = {"DEFAULT_CLOSED"} 
     
     @classmethod
     def poll(cls, context):
-        return context.material and THEBOUNTY_MaterialButtonsPanel.poll(context)
+        return context.material and TheBountyMaterialButtonsPanel.poll(context)
 
     def draw(self, context):
         self.layout.template_preview(context.material)
@@ -185,182 +189,181 @@ class YAF_MT_presets_ior_list(Menu):
             sl.menu(sm.bl_idname)
 
 
-class YAF_PT_shinydiffuse_diffuse(MaterialTypePanel, Panel):
+class TheBountyShinyDiffuse(TheBountyMaterialTypePanel, Panel):
     bl_label = "Diffuse reflection"
     material_type = 'shinydiffusemat'
 
     def draw(self, context):
         layout = self.layout
-        yaf_mat = active_node_mat(context.material)
+        mat = active_node_mat(context.material)
 
         split = layout.split()
         col = split.column()
-        col.prop(yaf_mat, "diffuse_color")
-        col.prop(yaf_mat, "emit")
-        layout.row().prop(yaf_mat, "diffuse_reflect", slider=True)
+        col.prop(mat, "diffuse_color")
+        col.prop(mat, "emit")
+        layout.row().prop(mat.bounty, "diffuse_reflect", slider=True)
 
         col = split.column()
         sub = col.column()
         sub.label(text="Reflectance model:")
-        sub.prop(yaf_mat, "brdf_type", text="")
+        sub.prop(mat.bounty, "brdf_type", text="")
         brdf = sub.column()
-        brdf.enabled = yaf_mat.brdf_type == "oren-nayar"
-        brdf.prop(yaf_mat, "sigma")
+        brdf.enabled = mat.bounty.brdf_type == "oren-nayar"
+        brdf.prop(mat.bounty, "sigma")
 
         layout.separator()
 
         box = layout.box()
-        #box.label(text="Transparency and translucency:")
         split = box.split()
         col = split.column()
-        col.prop(yaf_mat, "transparency", slider=True)
+        col.prop(mat.bounty, "transparency", slider=True)
         col = split.column()
-        col.prop(yaf_mat, "translucency", slider=True)
-        box.row().prop(yaf_mat, "transmit_filter", slider=True)
+        col.prop(mat, "translucency", slider=True)
+        box.row().prop(mat.bounty, "transmit_filter", slider=True)
 
 
-class YAF_PT_shinydiffuse_specular(MaterialTypePanel, Panel):
+class TheBountyShinySpecular(TheBountyMaterialTypePanel, Panel):
     bl_label = "Specular reflection"
     material_type = 'shinydiffusemat'
 
     def draw(self, context):
         layout = self.layout
-        yaf_mat = active_node_mat(context.material)
+        mat = active_node_mat(context.material)
         
         split = layout.split()
         col = split.column()
         col.label(text="Mirror color:")
-        col.prop(yaf_mat, "mirror_color", text="")
+        col.prop(mat, "mirror_color", text="")
 
         col = split.column()
-        col.prop(yaf_mat, "fresnel_effect")
+        col.prop(mat.bounty, "fresnel_effect")
         sub = col.column()
-        sub.enabled = yaf_mat.fresnel_effect
-        sub.prop(yaf_mat, "IOR_reflection", slider=True)
-        layout.row().prop(yaf_mat, "specular_reflect", slider=True)
+        sub.enabled = mat.bounty.fresnel_effect
+        sub.prop(mat.bounty, "IOR_reflection", slider=True)
+        layout.row().prop(mat.bounty, "specular_reflect", slider=True)
 
 
-class YAF_PT_glossy_diffuse(MaterialTypePanel, Panel):
+class TheBountyGlossyDiffuse(TheBountyMaterialTypePanel, Panel):
     bl_label = "Diffuse reflection"
     material_type = 'glossy', 'coated_glossy'
 
     def draw(self, context):
         layout = self.layout
-        yaf_mat = active_node_mat(context.material)
+        mat = active_node_mat(context.material)
 
         split = layout.split()
         col = split.column()
-        col.prop(yaf_mat, "diffuse_color")
+        col.prop(mat, "diffuse_color")
 
         col = split.column()
         ref = col.column(align=True)
         ref.label(text="Reflectance model:")
-        ref.prop(yaf_mat, "brdf_type", text="")
+        ref.prop(mat.bounty, "brdf_type", text="")
         sig = col.column()
-        sig.enabled = yaf_mat.brdf_type == "oren-nayar"
-        sig.prop(yaf_mat, "sigma")
-        layout.row().prop(yaf_mat, "diffuse_reflect", slider=True)
+        sig.enabled = mat.bounty.brdf_type == "oren-nayar"
+        sig.prop(mat.bounty, "sigma")
+        layout.row().prop(mat.bounty, "diffuse_reflect", slider=True)
 
 
-class YAF_PT_glossy_specular(MaterialTypePanel, Panel):
+class TheBountyGlossySpecular(TheBountyMaterialTypePanel, Panel):
     bl_label = "Specular reflection"
     material_type = 'glossy', 'coated_glossy'
 
     def draw(self, context):
         layout = self.layout
-        yaf_mat = active_node_mat(context.material)
+        mat = active_node_mat(context.material)
 
         split = layout.split()
         col = split.column()
-        col.prop(yaf_mat, "glossy_color")
+        col.prop(mat.bounty, "glossy_color")
         exp = col.column()
-        exp.enabled = yaf_mat.anisotropic == False
-        exp.prop(yaf_mat, "exponent")
+        exp.enabled = mat.bounty.anisotropic == False
+        exp.prop(mat.bounty, "exponent")
 
         col = split.column()
         sub = col.column(align=True)
-        sub.prop(yaf_mat, "anisotropic")
+        sub.prop(mat.bounty, "anisotropic")
         ani = sub.column()
-        ani.enabled = yaf_mat.anisotropic == True
-        ani.prop(yaf_mat, "exp_u")
-        ani.prop(yaf_mat, "exp_v")
-        layout.row().prop(yaf_mat, "glossy_reflect", slider=True)
-        layout.row().prop(yaf_mat, "as_diffuse")
+        ani.enabled = mat.bounty.anisotropic == True
+        ani.prop(mat.bounty, "exp_u")
+        ani.prop(mat.bounty, "exp_v")
+        layout.row().prop(mat.bounty, "glossy_reflect", slider=True)
+        layout.row().prop(mat.bounty, "as_diffuse")
 
         layout.separator()
 
-        if yaf_mat.mat_type == "coated_glossy":
+        if mat.bounty.mat_type == "coated_glossy":
             box = layout.box()
             box.label(text="Coated layer for glossy:")
             split = box.split()
             col = split.column()
-            col.prop(yaf_mat, "coat_mir_col")
+            col.prop(mat.bounty, "coat_mir_col")
             col = split.column(align=True)
             col.label(text="Fresnel reflection:")
-            col.prop(yaf_mat, "IOR_reflection")
+            col.prop(mat.bounty, "IOR_reflection")
             col.label()
 
 
-class YAF_PT_glass_real(MaterialTypePanel, Panel):
+class TheBountyRealGlass(TheBountyMaterialTypePanel, Panel):
     bl_label = "Real glass settings"
     material_type = 'glass', 'rough_glass'
 
     def draw(self, context):
         layout = self.layout
-        yaf_mat = active_node_mat(context.material)
+        mat = active_node_mat(context.material)
 
         layout.label(text="Refraction and Reflections:")
         split = layout.split()
         col = split.column()
-        col.prop(yaf_mat, "IOR_refraction")
+        col.prop(mat.bounty, "IOR_refraction")
 
         col = split.column()
         col.menu("YAF_MT_presets_ior_list", text=bpy.types.YAF_MT_presets_ior_list.bl_label)
 
         split = layout.split()
         col = split.column(align=True)
-        col.prop(yaf_mat, "absorption")
-        col.prop(yaf_mat, "absorption_dist")
+        col.prop(mat.bounty, "absorption")
+        col.prop(mat.bounty, "absorption_dist")
 
         col = split.column(align=True)
         col.label(text="Dispersion:")
-        col.prop(yaf_mat, "dispersion_power")
+        col.prop(mat.bounty, "dispersion_power")
 
-        if yaf_mat.mat_type == "rough_glass":
-            box = layout.box()
-            box.label(text="Glass roughness:")
-            box.row().prop(yaf_mat, "refr_roughness", slider=True)
+        if mat.bounty.mat_type == "rough_glass":
+            row = layout.row()
+            #box.label(text="Glass roughness:")
+            row.prop(mat.bounty, "refr_roughness",text="Roughness exponent", slider=True)
 
 
-class YAF_PT_glass_fake(MaterialTypePanel, Panel):
+class TheBountyFakeGlass(TheBountyMaterialTypePanel, Panel):
     bl_label = "Fake glass settings"
     material_type = 'glass', 'rough_glass'
 
     def draw(self, context):
         layout = self.layout
-        yaf_mat = active_node_mat(context.material)
+        mat = active_node_mat(context.material)
 
         split = layout.split()
         col = split.column()
-        col.prop(yaf_mat, "filter_color")
+        col.prop(mat.bounty, "filter_color")
         col = split.column()
-        col.prop(yaf_mat, "glass_mir_col")
-        layout.row().prop(yaf_mat, "glass_transmit", slider=True)
-        layout.row().prop(yaf_mat, "fake_shadows")
+        col.prop(mat.bounty, "glass_mir_col")
+        layout.row().prop(mat.bounty, "glass_transmit", slider=True)
+        layout.row().prop(mat.bounty, "fake_shadows")
 
 
-class YAF_PT_blend_(MaterialTypePanel, Panel):
+class TheBountyBlend(TheBountyMaterialTypePanel, Panel):
     bl_label = "Blend material settings"
     material_type = 'blend'
 
     def draw(self, context):
         layout = self.layout
-        yaf_mat = active_node_mat(context.material)
+        mat = active_node_mat(context.material)
 
         split = layout.split()
         col = split.column()
         col.label(text="")
-        col.prop(yaf_mat, "blend_value", slider=True)
+        col.prop(mat.bounty, "blend_value", slider=True)
 
         layout.separator()
 
@@ -369,52 +372,52 @@ class YAF_PT_blend_(MaterialTypePanel, Panel):
         split = box.split()
         col = split.column()
         col.label(text="Material one:")
-        col.prop(yaf_mat, "material1", text="")
+        col.prop(mat.bounty, "blendmaterial1", text="")
 
         col = split.column()
         col.label(text="Material two:")
-        col.prop(yaf_mat, "material2", text="")
+        col.prop(mat.bounty, "blendmaterial2", text="")
 
-class YAF_MT_sss_presets(Menu):
+class TheBountyScatteringPresets(Menu):
     bl_label = "Scattering Presets"
     preset_subdir = "thebounty/sss"
     preset_operator = "script.execute_preset"
     COMPAT_ENGINES = {'THEBOUNTY'}
     draw = Menu.draw_preset
     
-class YAF_PT_translucent(MaterialTypePanel, Panel):
+class TheBountyTranslucent(TheBountyMaterialTypePanel, Panel):
     bl_label = "Translucent"
     material_type = 'translucent'
 
     def draw(self, context):
         layout = self.layout
-        yaf_mat = active_node_mat(context.material)
+        mat = active_node_mat(context.material)
                 
         #
         split = layout.split()
         col = split.column()
         #col = layout.box()
-        col.prop(yaf_mat, "diffuse_color")
-        col.prop(yaf_mat, "diffuse_reflect", text="Diff. Reflect",slider=True)
+        col.prop(mat, "diffuse_color")
+        col.prop(mat.bounty, "diffuse_reflect", text="Diff. Reflect",slider=True)
         col = split.column()     
-        col.prop(yaf_mat, "glossy_color")#Glossy color")
-        col.prop(yaf_mat, "glossy_reflect", text="Gloss. Reflect",slider=True)
+        col.prop(mat.bounty, "glossy_color")#Glossy color")
+        col.prop(mat.bounty, "glossy_reflect", text="Gloss. Reflect",slider=True)
         row= layout.row()
-        row.prop(yaf_mat, "sssSpecularColor")
-        layout.prop(yaf_mat, "exponent", text="Specular Exponent")
+        row.prop(mat.bounty, "sssSpecularColor")
+        layout.prop(mat.bounty, "exponent", text="Specular Exponent")
         
         
-class YAF_PT_sss(MaterialTypePanel, Panel):
+class TheBountySubSurfaceScattering(TheBountyMaterialTypePanel, Panel):
     bl_label = "SubSurface"
     material_type = 'translucent'
 
     def draw(self, context):
         layout = self.layout
-        yaf_mat = active_node_mat(context.material)
+        mat = active_node_mat(context.material)
         ##
         row = layout.row()
         row.label("SSS Presets")
-        row.menu("YAF_MT_sss_presets", text=bpy.types.YAF_MT_sss_presets.bl_label)
+        row.menu("TheBountyScatteringPresets", text=bpy.types.TheBountyScatteringPresets.bl_label)
         # this operator's is not need, you can use material presets for save SSS presets
         #row.operator("yafaray.preset_add", text="", icon='ZOOMIN')
         #row.operator("yafaray.preset_add", text="", icon='ZOOMOUT').remove_active = True
@@ -422,14 +425,14 @@ class YAF_PT_sss(MaterialTypePanel, Panel):
         split = layout.split()
         col = split.column()        
         
-        col.prop(yaf_mat, "sssSigmaS", text="Scatter color")
-        col.prop(yaf_mat, "sssSigmaS_factor")
-        col.prop(yaf_mat, "phaseFuction")
+        col.prop(mat.bounty, "sssSigmaS", text="Scatter color")
+        col.prop(mat.bounty, "sssSigmaS_factor")
+        col.prop(mat.bounty, "phaseFuction")
                 
         col = split.column()
-        col.prop(yaf_mat, "sssSigmaA", text="Absorption color")
-        col.prop(yaf_mat, "sss_transmit", text="Transmit")
-        col.prop(yaf_mat, "sssIOR")
+        col.prop(mat.bounty, "sssSigmaA", text="Absorption color")
+        col.prop(mat.bounty, "sss_transmit", text="Transmit")
+        col.prop(mat.bounty, "sssIOR")
               
 
 if __name__ == "__main__":  # only for live edit.
