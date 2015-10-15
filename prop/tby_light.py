90dd0b66629d4cf066503d48d7f3b89948c3d375
povmaniaco <thebountyrenderer@gmail.com>
2015-10-16 00:02:50 +0200

    Delete some code on light property file, unused after regression code. Clean 'yaf' prefix in some variable names.

--- prop/tby_light.py
+++ prop/tby_light.py
@@ -27,31 +27,9 @@ from bpy.props import (EnumProperty,
                        PointerProperty
                        )
 
-enum_lamp_type = (
-    ('POINT', "Point", "Omnidirectional point light source"),
-    ('SUN',   "Sun",   "Constant direction parallel ray light source"),
-    ('SPOT',  "Spot",  "Directional cone light source"),
-    ('IES',   "IES",   "Directional cone light source from ies file"),
-    ('AREA',  "Area",  "Directional area light source"),
-    ('DIRECTIONAL', "Directional", "Directional Sun light"),
-)
-switchLampType = {
-    'IES': 'SPOT',
-    'DIRECTIONAL': 'SUN',
-    'POINT' : 'POINT',
-    'SUN':'SUN',
-    'AREA':'AREA',
-    'SPOT':'SPOT'
-}
-
-def sync_light_type(self, context):
-    lamp = context.lamp
-    if lamp is not None:
-        lamp.type = switchLampType.get(context.lamp.bounty.lamp_type)
-
 def set_shadow_method(self, context):
     lamp = context.lamp
-    if context.lamp.bounty.yaf_show_dist_clip:
+    if context.lamp.bounty.show_dist_clip:
         lamp.shadow_method = 'BUFFER_SHADOW'
     else:
         lamp.shadow_method = 'RAY_SHADOW'
@@ -65,12 +43,13 @@ def sync_sphere_light(self, context):
     else:
         lamp.use_sphere = False
         lamp.distance = 10
-
+   
+#######
 def sync_with_distance(self, context):
     lamp = context.lamp
-    radius = context.lamp.bounty.yaf_sphere_radius
+    radius = context.lamp.bounty.sphere_radius
     if radius != context.lamp.distance:
-        context.lamp.distance = context.lamp.bounty.yaf_sphere_radius 
+        context.lamp.distance = context.lamp.bounty.sphere_radius 
        
 class TheBountyLightProperties(bpy.types.PropertyGroup):
     #
@@ -78,15 +57,8 @@ class TheBountyLightProperties(bpy.types.PropertyGroup):
             name="Node Tree",
             description="Name of the node tree for this light",
             default=""
-    ) 
-    lamp_type = EnumProperty(
-            name="Light type",
-            description="Type of lamp",
-            items=enum_lamp_type,
-            default="POINT", 
-            update=sync_light_type
-    )    
-    yaf_energy = FloatProperty(
+    )
+    energy = FloatProperty(
             name="Power",
             description="Intensity multiplier for color",
             min=0.0, max=10000.0,
@@ -96,20 +68,22 @@ class TheBountyLightProperties(bpy.types.PropertyGroup):
             name="Use Sphere",
             description="Use sphere object for light",
             default=False,
-            update = sync_sphere_light # call_update_sphere
+            update = sync_sphere_light
     )    
-    yaf_sphere_radius = FloatProperty(
+    sphere_radius = FloatProperty(
             name="Radius",
             description="Radius of the sphere light",
             min=0.01, max=10000.0,
             soft_min=0.01, soft_max=100.0,
             default=1.0, update=sync_with_distance
-    )    
-    #cls.directional = BoolProperty(
-    #    name="Directional",
-    #    description="Directional sunlight type, like 'spot' (for concentrate photons at area)",
-    #    default=False
-    #)
+    )
+    shadows_size = FloatProperty(
+            name="Shadows soft size",
+            description="Radius of directional cone",
+            min=0.01, max=10000.0,
+            soft_min=0.01, soft_max=100.0,
+            default=10.0 #, update=sync_with_size
+    )      
     create_geometry = BoolProperty(
             name="Create and show geometry",
             description="Creates a visible geometry in the dimensions of the light during the render",
@@ -153,13 +127,13 @@ class TheBountyLightProperties(bpy.types.PropertyGroup):
             subtype='FILE_PATH',
             default=""
     )    
-    yaf_samples = IntProperty(
+    samples = IntProperty(
             name="Samples",
             description="Number of samples to be taken for direct lighting",
             min=0, max=512,
             default=16
     )    
-    yaf_show_dist_clip = BoolProperty(
+    show_dist_clip = BoolProperty(
             name="Show clipping",
             description="Show clip start and clip end settings for spot lamp in 3D view",
             default=False, 
