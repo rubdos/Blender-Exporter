8b497318bff6a6d53df62ea91cefd1536d41a555
Ruben De Smet <ruben.de.smet@rubdos.be>
2016-02-06 16:59:51 +0100

    Abandoning CFFI until cffi can read the Blender primitives

--- native/tby_export.cpp
+++ native/tby_export.cpp
@@ -1,1 +1,15 @@
 #include "tby_export.hpp"
+
+const char* TheBountyRenderEngine::bl_label = "TheBounty Render";
+const char* TheBountyRenderEngine::bl_idname = "THEBOUNTY";
+const char* TheBountyRenderEngine::tag = "";
+const bool TheBountyRenderEngine::bl_use_preview = true;
+
+TheBountyRenderEngine::TheBountyRenderEngine() {
+}
+
+void TheBountyRenderEngine::update(void* data, void* scene) {
+}
+
+void TheBountyRenderEngine::render(void* scene) {
+}
--- native/tby_export.hpp
+++ native/tby_export.hpp
@@ -1,3 +1,13 @@
 class TheBountyRenderEngine {
+public:
+    TheBountyRenderEngine();
+    static const char* bl_label;
+    static const char* bl_idname;
+    static const bool bl_use_preview;
+    static const char* tag;
+
+    void update(void* data, void* scene);
+    void render(void* scene);
+private:
 };
 
