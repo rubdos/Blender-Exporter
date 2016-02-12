0e3eb4016e5bb59702fbc9ea216534679356657a
Ruben De Smet <ruben.de.smet@rubdos.be>
2016-02-12 14:42:03 +0100

    Add copyright notices for povmaniac

--- src/blender/blender_scene.hpp
+++ src/blender/blender_scene.hpp
@@ -1,5 +1,6 @@
 /**
  *  Copyright 2016 Ruben De Smet
+ *                 Pedro Alcaide
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
